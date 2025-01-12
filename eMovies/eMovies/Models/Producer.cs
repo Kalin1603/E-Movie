9fafa986726d4aec8c9da2a4bb549376a442c2a1
Kalin1603 <kalintodorov03@gmail.com>
2025-01-12 13:51:35 +0200

    Updated Producers/Index.cshtml, Actors/Index.cshtml and _Layout.cshtml

--- eMovies/eMovies/Models/Producer.cs
+++ eMovies/eMovies/Models/Producer.cs
@@ -7,17 +7,21 @@ namespace eMovies.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "First name")]
         [Required(ErrorMessage = "First name is required.")]
         [MaxLength(30, ErrorMessage = "First name cannot exceed 30 characters.")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last name")]
         [Required(ErrorMessage = "Last name is required.")]
         [MaxLength(30, ErrorMessage = "Last name cannot exceed 30 characters.")]
         public string LastName { get; set; }
 
+        [Display(Name = "Biography")]
         [MaxLength(1000, ErrorMessage = "Bio cannot exceed 100 characters.")]
         public string Bio { get; set; }
 
+        [Display(Name = "Profile picture")]
         [Required(ErrorMessage = "Profile picture URL is required.")]
         [Url(ErrorMessage = "Invalid URL format.")]
         public string ProfilePictureURL { get; set; }
